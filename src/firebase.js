7ccf3747353aa98d64ee5bb9a361966d624a3322
Molonia2002 <114032831+Molonia2002@users.noreply.github.com>
2025-08-29 08:00:45 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIza",
   authDomain: "mini-chat-app-b3d70.firebaseapp.com",
   projectId: "mini-chat-app-b3d70",
-  storageBucket: "mini-chat-app-b3d70.firebasestorage.app",
+  storageBucket: "",
   messagingSenderId: "",
   appId: "",
   measurementId: "G-QP28M1KG7P"
